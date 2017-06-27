afe41a65b9cc71c288e5ea3e2b4f3e914e842564
andydvmware <andyd@vmware.com>
2017-06-27 22:04:10 +0100

    Removed Redundant Files

--- removeandimportpublic.ps1
+++
@@ -1,5 +0,0 @@
-remove-module powervrops -erroraction ignore
-remove-module PowervROpsPublic -erroraction ignore
-remove-module credentials -erroraction ignore
-import-module 'C:\Users\taguser\Documents\GitHub\PowervROps\PowervROpsPublic.psm1'
-import-module 'C:\Users\taguser\Documents\GitHub\PowervROps\credentials.psm1'
